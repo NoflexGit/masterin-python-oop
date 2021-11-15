c3e88e5357388d2537b349e3ae509e21fae46612
NoflexGit <untroman@mail.ru>
2021-11-15 08:38:34 +0300

    Basic inheritance

--- main.py
+++ main.py
@@ -24,6 +24,15 @@ class Item:
     def __repr__(self) -> str:
         return f"Item('{self.name}', {self.price}, {self.quantity})"
 
+    @staticmethod
+    def is_int(num):
+        if isinstance(num, float):
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
     @classmethod
     def get_items_from_csv(cls):
         with open('items.csv', 'r') as f:
@@ -38,5 +47,14 @@ class Item:
             )
 
 
-Item.get_items_from_csv()
-print(Item.all)
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity=0, broken_items=0):
+        super().__init__(name, price, quantity)
+
+        assert broken_items >= 0, f"Broken items {broken_items} is not greater or equal to zero!"
+
+        self.broken_items = broken_items
+
+
+phone1 = Phone('Iphone 5s', 299, 1, 0)
+print(phone1.name)
