5e20e05b01b8ce09b2b7ae2b2886f36581ad9ee2
NoflexGit <untroman@mail.ru>
2021-11-15 08:15:30 +0300

    Working with class methods

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
+import csv
+
+
 class Item:
+    pay_rate = 0.8
+    all = []
+
     def __init__(self, name: str, price: float, quantity=0):
         assert price >= 0, f"Price {price} is not greater or equal to zero!"
         assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
 
-        self.price = name
+        self.name = name
         self.price = price
         self.quantity = quantity
+
+        Item.all.append(self)
 
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
-item1 = Item('IPhone 13 Pro', -1, 10)
-item2 = Item('IPad Pro', 1099, 3)
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+    def __repr__(self) -> str:
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
+    @classmethod
+    def get_items_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=float(item.get('quantity')),
+            )
+
+
+Item.get_items_from_csv()
+print(Item.all)
